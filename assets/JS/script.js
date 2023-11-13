bd0008414460c4a2b8d8835504ce37ca6adf6b7b
unknown <dannysaravia100@gmail.com>
2023-11-13 10:01:50 -0500

    added API key to JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,5 @@
 async function fetchCrypto() {
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={3d225cfd565e3133c3357232d792999c}';
   
     try {
       const response = await fetch(apiUrl, { method: 'GET' });
